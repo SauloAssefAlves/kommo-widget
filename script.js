a8a1b4404480780c95268028e1ee6ecf3ee1b749
SauloAssefAlves <105947903+SauloAssefAlves@users.noreply.github.com>
2024-12-27 19:17:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.Kommo.init({
-  token: "SEU_ACCESS_TOKEN", // Substitua pelo seu Access Token
-  widgetId: "ID_DO_WIDGET", // Substitua pelo seu Widget ID
+  token: "a6hNjjC7hRvyuzqenLoczOQSu3da3MpIBiHdIku7wAGGdyggMFiIJMWwk7CA2HvZ", // Substitua pelo seu Access Token
+  widgetId: "93fd178e-9bab-43f1-a751-65c90c026320", // Substitua pelo seu Widget ID
   openButton: "#open-chat", // ID do botão que irá abrir o chat
   openOnLoad: false, // O widget não abre automaticamente
 });
