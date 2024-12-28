f156f64f7e5673f1a3e96b4563cd75a7f9d53a99
Saulo <saulo.assef98@gmail.com>
2024-12-28 17:22:20 -0300

    asassa

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import leadController from "./controllers/leadController.js";
+import leadController from "./public/controllers/leadController.js";
 
 // Configuração das variáveis de ambiente
 dotenv.config();
